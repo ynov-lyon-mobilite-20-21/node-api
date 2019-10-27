77f380adf02216eae94554bab4a82f0b4491235a
Maxence MOTTARD <mottard.maxence@gmail.com>
2019-10-27 17:46:10 +0000

    Setup server with express, mongoose & Users and authentication features

---
+++ src/routes/AuthRouter.js
@@ -0,0 +1,43 @@
+const Router = require('./Router');
+const AuthService = require('../services/AuthService');
+const RefreshTokenService = require('../services/RefreshTokenService');
+const UserService = require('../services/UserService');
+
+class AuthRouter extends Router {
+
+    constructor () {
+        super();
+
+        this.post({
+            endpoint: '/auth',
+            callback: this.authentication.bind(this),
+            requiredFields: [ { name: 'mail', format: 'email' }, { name: 'password' } ]
+        })
+
+        this.post({
+            endpoint: '/auth/refresh'
+        })
+    }
+
+    async authentication(req) {
+        const user = await UserService.findOneBy({ mail: req.body.mail });
+
+        if (!user || user.password !== UserService.encryptPassword(req.body.password, user.mail) ) {
+            return this.response(401, {}, {
+                code: "BAD_CREDENTIALS",
+                message: "Invalid password or mail"
+            })
+        }
+
+        const refreshToken = await RefreshTokenService.createRefreshToken(user.id);
+        const token = await AuthService.createTokens(user.id);
+
+        this.response(200, { token, refreshToken: refreshToken.token })
+    }
+
+    // async refresh(req) {
+    //     const
+    // }
+}
+
+module.exports = new AuthRouter();
---
+++ src/routes/UserRouter.js
@@ -0,0 +1,84 @@
+const Router = require('./Router')
+const UserService = require('../services/UserService')
+
+class UserRouter extends Router {
+
+    constructor () {
+        super()
+
+        this.post({
+            endpoint: '/user/',
+            callback: this.createUser.bind(this),
+            requiredFields: [{name: 'mail', format: 'email'}]
+        })
+
+        this.post({
+            endpoint: '/user/active',
+            callback: this.activeUser.bind(this),
+            requiredFields: [{name: 'userId'}, {name: 'activationKey'}, {name: 'password'}]
+        })
+
+        this.get({
+            endpoint: '/user',
+            callback: this.getUser.bind(this),
+            authentication: true
+        })
+
+        this.put({
+            endpoint: '/user',
+            callback: this.updateUser.bind(this)
+        })
+
+        this.delete({
+            endpoint: '/user',
+            callback: this.deleteUser.bind(this)
+        })
+    }
+
+    async createUser (req) {
+        const userCreation = await UserService.createUser(req.body)
+
+        if (!userCreation.success) {
+            return this.response(400, {}, {code: userCreation.code})
+        }
+
+        this.response(200, userCreation.data)
+    };
+
+    async activeUser (req) {
+        const userActivation = await UserService.activeUser(req.body)
+
+        if (!userActivation.success) {
+            return this.response(400, {}, {code: userActivation.code})
+        }
+
+        this.response(200, {message: 'User is now active'})
+    };
+
+    async deleteUser (req) {
+        const userDeletion = await UserService.delete({ _id: req.user.id })
+
+        if (!userDeletion) {
+            this.response(400, {}, { code: "CANNOT_DELETE_USER" })
+        }
+
+        this.response(200, { message: `User ${req.user.object.id} is now deleted.` })
+    };
+
+    async updateUser (req, res) {
+        const userUpdate = await UserService.updateOne({ _id: req.user.id}, req.body)
+
+        if (!userUpdate) {
+            this.response(400, {}, { code: "CANNOT_UPDATE_USER" })
+        }
+
+        this.response(200, { message: `User ${req.user.object.id} was updated.` })
+    };
+
+    getUser (req) {
+        this.response(200, req.user.object)
+    };
+
+}
+
+module.exports = new UserRouter()
