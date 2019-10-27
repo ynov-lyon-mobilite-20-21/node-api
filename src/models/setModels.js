ece98e6422176cdfda25fe486147a2c3134e6e25
MaxenceMottard <mottard.maxence@gmail.com>
2019-10-27 10:20:05 +0100

    Create Model

--- src/models/setModels.js
+++ src/models/setModels.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = require('./UserModel')
+const refreshToken = require('./RefreshToken')
 
 function setModels() {
     mongoose.model('User', userSchema)
+    mongoose.model('RefreshToken', refreshToken)
 }
 
 module.exports = setModels
