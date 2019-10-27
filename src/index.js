093d80d1bd927ea208f36038228d6d3210159788
MaxenceMottard <mottard.maxence@gmail.com>
2019-10-27 09:08:33 +0100

    Setup Mongoose, use UserRouter, set models

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
-const router = require('./routes/router');
+const mongoose = require('mongoose');
+const setModels = require('./models/setModels');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then( () => console.log('Connected to MongoDB') )
+    .catch( (err) => console.log(err) )
+setModels();
+
 app.use(express.json())
-app.use(router)
+app.use('/api/user', require('./routes/UserRouter').getRouter())
 
 const port = process.env.PORT;
 app.listen(port, () => {
