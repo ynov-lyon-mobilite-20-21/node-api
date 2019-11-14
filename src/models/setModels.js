4594a5139d89a9a0d16a1631f66fe3760dc133ce
MaxenceMottard <mottard.maxence@gmail.com>
2019-11-14 21:36:23 +0100

    Fix setModel & Require ProductSchema

--- src/models/setModels.js
+++ src/models/setModels.js
@@ -1,11 +1,13 @@
-sconst mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const userSchema = require('./UserModel')
 const refreshToken = require('./RefreshToken')
+const productSchema = require('./ProductModel')
 
 function setModels() {
     mongoose.model('User', userSchema)
     mongoose.model('RefreshToken', refreshToken)
+    mongoose.model('Product', productSchema)
 }
 
 module.exports = setModels
