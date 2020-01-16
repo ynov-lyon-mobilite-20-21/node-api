fe0f265fcadb27cd0fa5cfdee3d126270b860127
MaxenceMottard <mottard.maxence@gmail.com>
2020-01-16 09:54:15 +0100

    Add address fields to UserModel

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  mail: {type: 'string', unique: true, required: true},
-  password: {type: 'string', select: false},
-  firstName: {type: 'string'},
-  lastName: {type: 'string'},
-  birthday: {type: 'number'},
-  registrationDate: {type: 'number'},
-  active: {type: 'boolean', required: true},
-  isAdmin: {type: 'boolean', default: false},
-  activationKey: {type: 'string'},
-  __v: {type: Number, select: false},
+    mail: {type: 'string', unique: true, required: true},
+    password: {type: 'string', select: false},
+    firstName: {type: 'string'},
+    lastName: {type: 'string'},
+    registrationDate: {type: 'number'},
+    active: {type: 'boolean', required: true},
+    isAdmin: {type: 'boolean', default: false},
+    activationKey: {type: 'string'},
+    address: { type: 'string' },
+    postalCode: { type: 'string' },
+    city: { type: 'string' },
+    __v: {type: Number, select: false},
 })
 
 module.exports = userSchema
