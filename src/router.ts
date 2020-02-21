893c7a0f0a78295dd656e8eed4f435c922ff6f97
Maxence MOTTARD <mottard.maxence@gmail.com>
2020-02-21 15:37:58 +0000

    Update router.ts to fix id parameters

--- src/router.ts
+++ src/router.ts
@@ -29,14 +29,14 @@ exRouter.post('/auth/refresh', refreshUserToken);
 
 /*   PRODUCTS   */
 exRouter.post('/products', [userMiddlewares.isAuthenticated, userMiddlewares.isAdmin], postProduct);
-exRouter.get('/products/id', getOneProductById);
+exRouter.get('/products/:id', getOneProductById);
 exRouter.get('/products', getAllProducts);
-exRouter.delete('/products/id', [userMiddlewares.isAuthenticated, userMiddlewares.isAdmin], deleteProductById);
+exRouter.delete('/products/:id', [userMiddlewares.isAuthenticated, userMiddlewares.isAdmin], deleteProductById);
 
 /*   IMAGES   */
 exRouter.post('/images', [userMiddlewares.isAuthenticated, userMiddlewares.isAdmin], postImage);
-exRouter.get('/images/id', getOneImageById);
+exRouter.get('/images/:id', getOneImageById);
 exRouter.get('/images', getAllImages);
-exRouter.delete('/images/id', [userMiddlewares.isAuthenticated, userMiddlewares.isAdmin], deleteImageById);
+exRouter.delete('/images/:id', [userMiddlewares.isAuthenticated, userMiddlewares.isAdmin], deleteImageById);
 
 export default (): Router => exRouter;
