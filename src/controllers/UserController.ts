479c81497f6bb0b1fb8febc1c2e0e5cecca534e1
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2021-03-09 00:35:25 +0100

    refactor: postUser and userActivation of UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,17 +1,13 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Request, Response } from 'express';
-import Crypto from 'crypto';
 import moment from 'moment';
 import {
-  deleteOnyBy,
-  findManyBy, findOneBy, saveData, updateOneBy,
+  deleteOnyBy, findManyBy, findOneBy, saveData, updateOneBy,
 } from '../services/MongooseService';
 import { User, UserModel } from '../models/UserModel';
-import { sendRegistrationMail } from '../services/MailService';
-import { encryptPassword } from '../services/UserService';
+import { sendInactiveUserAccountExistMail, sendRegistrationMail } from '../services/MailService';
+import { createActivationKey, encryptPassword } from '../services/AuthService';
 import { createStripeCustomer } from '../services/StripeService';
-
-const { CLIENT_HOSTNAME } = process.env;
 
 export const postUser = async (req: Request, res: Response): Promise<void> => {
   const {
@@ -21,47 +17,69 @@ export const postUser = async (req: Request, res: Response): Promise<void> => {
   if (!mail) {
     res.status(400).json({
       data: {},
-      error: { code: 'EMAIL_REQUIRED' },
+      error: {
+        code: 'EMAIL_REQUIRED',
+        message: 'Please fill email field, this field is required.',
+      },
     });
-
     return;
   }
+
   if (!password) {
     res.status(400).json({
       data: {},
-      error: { code: 'PASSWORD_REQUIRED' },
+      error: {
+        code: 'PASSWORD_REQUIRED',
+        message: 'Please fill password field, this field is required.',
+      },
     });
 
     return;
   }
+
   if (!firstName) {
     res.status(400).json({
       data: {},
-      error: { code: 'FIRSTNAME_REQUIRED' },
+      error: {
+        code: 'FIRSTNAME_REQUIRED',
+        message: 'Please fill firstname field, this field is required.',
+      },
     });
 
     return;
   }
+
   if (!lastName) {
     res.status(400).json({
       data: {},
-      error: { code: 'LASTNAME_REQUIRED' },
+      error: {
+        code: 'LASTNAME_REQUIRED',
+        message: 'Please fill lastname field, this field is required.',
+      },
     });
 
     return;
   }
+
   if (!promotion) {
     res.status(400).json({
       data: {},
-      error: { code: 'PROMOTION_REQUIRED' },
+      error: {
+        code: 'PROMOTION_REQUIRED',
+        message: 'Please fill promotion field, this field is required.',
+      },
     });
 
     return;
   }
+
   if (!formation) {
     res.status(400).json({
       data: {},
-      error: { code: 'FORMATION_REQUIRED' },
+      error: {
+        code: 'FORMATION_REQUIRED',
+        message: 'Please fill formation field, this field is required.',
+      },
     });
 
     return;
@@ -69,66 +87,172 @@ export const postUser = async (req: Request, res: Response): Promise<void> => {
 
   let user = await findOneBy<User>({ model: UserModel, condition: { mail } });
 
-  if (user && user.isActive) {
-    res.status(400).json({
-      data: {},
-      error: { code: 'USER_ALREADY_EXISTS' },
-    });
-
-    return;
-  }
-
   if (user && !user.isActive) {
-    res.status(400).json({
-      data: {},
-      error: { code: 'USER_INACTIVE', message: 'Activation link resent, check your email' },
-    });
+    const newActivationKey = createActivationKey();
 
-    const newActivationKey = Crypto.randomBytes(50).toString('hex');
-    await updateOneBy<User>({ model: UserModel, condition: { mail: user.mail }, set: { activationKey: newActivationKey } });
-    const activationLink = `${CLIENT_HOSTNAME}/users/activation?u=${user._id}&k=${user.activationKey}`;
-    await sendRegistrationMail(user.mail, activationLink);
-
-    return;
-  }
-
-  if (!user) {
-    const activationKey = Crypto.randomBytes(50).toString('hex');
-    const encryptedPassword = await encryptPassword(password);
-
-    user = await saveData<User>({
+    await updateOneBy<User>({
       model: UserModel,
-      params: {
-        mail, password: encryptedPassword, firstName, lastName, promotion, formation, activationKey, isActive: true,
+      condition: {
+        mail: user.mail,
+      },
+      set: {
+        activationKey: newActivationKey,
       },
     });
 
-    const activationLink = `${CLIENT_HOSTNAME}/users/activation?u=${user._id}&k=${user.activationKey}`;
-    await sendRegistrationMail(user.mail, activationLink);
+    const isEmailSentSuccessfully = await sendInactiveUserAccountExistMail(user.mail, newActivationKey);
 
-    if (!user) {
-      res.status(400).json({
+    if (!isEmailSentSuccessfully) {
+      console.log(isEmailSentSuccessfully);
+      res.status(500).json({
         data: {},
-        error: { code: 'UNKNOWN_ERROR', message: 'An error has occured while user creation in database' },
+        error: {
+          code: 'UNKNOWN_ERROR',
+          message: 'An error has occurred while send validation email to user.',
+        },
       });
 
       return;
     }
 
-    res.status(200).json({
-      data: {
-        code: 'OK',
-        message: 'Account created successfully',
+    res.status(400).json({
+      data: {},
+      error: {
+        code: 'USER_INACTIVE',
+        message: 'Your account already exist but as inactive. A new activation link is send again to you, check your email.',
+      },
+    });
+    return;
+  }
+
+  if (user && user.isActive) {
+    res.status(400).json({
+      data: {},
+      error: {
+        code: 'USER_ALREADY_EXISTS',
+        message: 'Your account is already exist and activated. Please login.',
       },
     });
 
     return;
   }
 
-  res.status(400).json({
-    data: {},
-    error: { code: 'UNKNOWN_ERROR' },
+  if (user) {
+    res.status(400).json({
+      data: {},
+      error: {
+        code: 'UNKNOWN_ERROR',
+        message: 'Your account seems to already exist but not activated or inactive. Your account is possibly corrupted or banned Please contact the administrator.',
+      },
+    });
+
+    return;
+  }
+
+  const activationKey = createActivationKey();
+  const encryptedPassword = await encryptPassword(password);
+
+  const isEmailSentSuccessfully = await sendRegistrationMail(mail, activationKey);
+
+  if (!isEmailSentSuccessfully) {
+    res.status(500).json({
+      data: {},
+      error: {
+        code: 'UNKNOWN_ERROR',
+        message: 'An error has occurred while send validation email to user.',
+      },
+    });
+
+    return;
+  }
+
+  user = await saveData<User>({
+    model: UserModel,
+    params: {
+      mail,
+      password: encryptedPassword,
+      firstName,
+      lastName,
+      promotion,
+      formation,
+      activationKey,
+    },
   });
+
+  if (!user) {
+    res.status(500).json({
+      data: {},
+      error: {
+        code: 'UNKNOWN_ERROR',
+        message: 'An error has occurred while user creation in database',
+      },
+    });
+
+    return;
+  }
+
+  res.status(200).json({
+    data: user,
+  });
+};
+
+export const userActivation = async (req: Request, res: Response): Promise<void> => {
+  const { activationKey } = req.params;
+
+  const user = await findOneBy<User>({ model: UserModel, condition: { activationKey } });
+
+  if (!user) {
+    res.status(400).json({
+      data: {},
+      error: {
+        code: 'INVALID_ACTIVATION_KEY',
+        message: 'No user found with this activation key.',
+      },
+    });
+
+    return;
+  }
+
+  if (user.stripeId) {
+    res.status(400).json({
+      data: {},
+      error: {
+        code: 'ACCOUNT_ALREADY_ACTIVATED',
+        message: 'Your user is already active.',
+      },
+    });
+
+    return;
+  }
+
+  const customer = await createStripeCustomer(user);
+
+  if (!customer) {
+    res.status(500).json({
+      data: {},
+      error: {
+        code: 'UNKNOWN_STRIPE_ERROR',
+        message: 'An error has occurs while creating your account on stripe.',
+      },
+    });
+
+    return;
+  }
+
+  const { id: stripeId } = customer;
+
+  await updateOneBy<User>({
+    model: UserModel,
+    condition: { _id: user._id },
+    set: {
+      isActive: true,
+      activationKey: null,
+      registrationDate: moment().unix(),
+      stripeId,
+    },
+  });
+
+  res.redirect('https://via.placeholder.com/414x736?text=Application+redirection'); // TODO: update redirection
 };
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
@@ -168,48 +292,6 @@ export const getMe = async (req: Request, res: Response): Promise<void> => {
     data: user,
     error: {},
   });
-};
-
-export const userActivation = async (req: Request, res: Response): Promise<void> => {
-  const { userId, activationKey, password } = req.body;
-
-  const user = await findOneBy<User>({ model: UserModel, condition: { _id: userId } });
-
-  if (!user || user.isActive || user.activationKey !== activationKey) {
-    res.status(400).json({
-      data: {},
-      error: { code: 'UNKNOWN_ERROR' },
-    });
-
-    return;
-  }
-
-  const customer = await createStripeCustomer(user);
-
-  if (!customer) {
-    // TODO GERER L'ERREUR
-    return;
-  }
-
-  const { id } = customer;
-  const stripeId = id;
-  // const stripeId = null;
-
-  const encryptedPassword = await encryptPassword(password);
-
-  await updateOneBy<User>({
-    model: UserModel,
-    condition: { _id: userId },
-    set: {
-      password: encryptedPassword,
-      isActive: true,
-      activationKey: null,
-      registrationDate: moment().unix(),
-      stripeId,
-    },
-  });
-
-  res.status(204).send();
 };
 
 export const updateUser = async (req: Request, res: Response): Promise<void> => {
