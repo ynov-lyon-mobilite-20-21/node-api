40789ca945429ba3dfac25d12a6f4a23f28bc58b
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2021-03-10 14:00:56 +0100

    fix: NODE_ENV var

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,13 @@ import morganBody from 'morgan-body';
 import RefreshTokenCron from './crons/refresh-token-cron';
 
 const app: Application = express();
-const { NODE_ENV } = process.env;
 
-if (NODE_ENV === undefined) {
+if (process.env.NODE_ENV === undefined) {
   // eslint-disable-next-line global-require
   require('dotenv').config();
 }
 
-const { DB_URL, PORT } = process.env;
+const { NODE_ENV, DB_URL, PORT } = process.env;
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion,max-len
 mongoose.connect(DB_URL!, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
@@ -38,5 +37,5 @@ app.use('/api', router());
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server started. PORT : ${PORT}`);
+  console.log(`Server started. PORT : ${PORT} on environment ${NODE_ENV}`);
 });
