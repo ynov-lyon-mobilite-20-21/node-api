6e6b4eda727a5d82b4bcf6824e01b8292507b666
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2021-03-10 15:40:51 +0100

    refactor: improve security and responses of AuthController actions

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,52 +1,103 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types,no-console */
-/*  eslint-disable @typescript-eslint/explicit-function-return-type */
-
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/explicit-function-return-type */
 import { Request, Response } from 'express';
 import moment from 'moment';
-import Crypto from 'crypto';
 import { deleteOnyBy, findOneBy, updateOneBy } from '../services/MongooseService';
 import { RefreshToken, RefreshTokenModel } from '../models/RefreshTokenModel';
 import { User, UserModel } from '../models/UserModel';
-import { comparePassword, createRefreshTokenForUser, createTokenForUser } from '../services/AuthService';
-import { sendRegistrationMail } from '../services/MailService';
-
-const { CLIENT_HOSTNAME } = process.env;
+import {
+  comparePassword,
+  createActivationKey,
+  createRefreshTokenForUser,
+  createTokenForUser,
+} from '../services/AuthService';
+import { sendInactiveUserAccountExistMail } from '../services/MailService';
 
 export const login = async (req: Request, res: Response) => {
   const { mail, password } = req.body;
 
   const user = await findOneBy<User>({ model: UserModel, condition: { mail }, hiddenPropertiesToSelect: ['password'] });
   if (!user) {
-    return res.status(400).json({
-      error: { code: 'NO_USER', message: '' },
+    res.status(400).json({
+      error: {
+        code: 'UNKNOWN_USER',
+        message: 'User not found for these logins.',
+      },
       data: null,
     });
-  }
-
-  if (!user.isActive) {
-    res.status(403).json({
-      error: { code: 'USER_INACTIVE', message: 'Activation link resent, check your email' },
-      data: null,
-    });
-
-    const newActivationKey = Crypto.randomBytes(50).toString('hex');
-    await updateOneBy<User>({ model: UserModel, condition: { mail: user.mail }, update: { activationKey: newActivationKey } });
-    const activationLink = `${CLIENT_HOSTNAME}/users/activation?u=${user._id}&k=${user.activationKey}`;
-    await sendRegistrationMail(user.mail, activationLink);
 
     return;
   }
 
-  const passwordIsCorrect = await comparePassword({ password, storedPassword: user.password! });
-  if (!passwordIsCorrect) {
-    return res.status(401).json({
-      error: { code: 'BAD_CREDENTIALS', message: '' },
+  if (!user.isActive) {
+    const newActivationKey = createActivationKey();
+
+    const updatedUser = await updateOneBy<User>({ model: UserModel, condition: { mail: user.mail }, update: { activationKey: newActivationKey } });
+
+    if (!updatedUser) {
+      res.status(500).json({
+        error: {
+          code: 'UNKNOWN_ERROR',
+          message: 'An error has occurred while update user activationKey in database',
+        },
+        data: null,
+      });
+
+      return;
+    }
+
+    const isEmailSentSuccessfully = await sendInactiveUserAccountExistMail(updatedUser.mail, newActivationKey);
+
+    if (!isEmailSentSuccessfully) {
+      res.status(500).json({
+        error: {
+          code: 'UNKNOWN_ERROR',
+          message: 'An error has occurred while send validation email to user.',
+        },
+        data: null,
+      });
+
+      return;
+    }
+
+    res.status(403).json({
+      error: {
+        code: 'USER_INACTIVE',
+        message: 'Activation link resent.',
+      },
       data: null,
     });
+
+    return;
+  }
+
+  const userPassword = user.password || '';
+  const isCorrectPassword = await comparePassword({ password, storedPassword: userPassword });
+  if (!isCorrectPassword) {
+    res.status(401).json({
+      error: {
+        code: 'BAD_CREDENTIALS',
+        message: 'The password is incorrect.',
+      },
+      data: null,
+    });
+
+    return;
   }
 
   const newToken = await createTokenForUser(user);
   const newRefreshToken = await createRefreshTokenForUser(user);
+
+  if (!newRefreshToken) {
+    res.status(500).json({
+      error: {
+        code: 'UNKNOWN_ERROR',
+        message: 'An error has occurred while saving the refresh token.',
+      },
+      data: null,
+    });
+
+    return;
+  }
 
   res.status(200).json({
     error: null,
@@ -56,58 +107,87 @@ export const login = async (req: Request, res: Response) => {
 
 export const refreshUserToken = async (req: Request, res: Response) => {
   const { refreshToken } = req.body;
-  const refreshTokenObj = await findOneBy<RefreshToken>({ model: RefreshTokenModel, condition: { token: refreshToken } });
 
-  if (
-    !refreshTokenObj
-        || !refreshTokenObj!.isActive
-        || refreshTokenObj!.expirationDate < moment().unix()
-  ) {
-    return res.status(400).json({
-      error: { code: 'INVALID_TOKEN', message: '' },
+  if (!refreshToken) {
+    res.status(400).json({
+      error: {
+        code: 'REFRESH_TOKEN_REQUIRED',
+        message: 'Please fill refreshToken field, this field is required.',
+      },
       data: null,
     });
+    return;
+  }
+
+  const refreshTokenObj = await findOneBy<RefreshToken>({ model: RefreshTokenModel, condition: { token: refreshToken } });
+
+  if (!refreshTokenObj
+    || (!refreshTokenObj.isActive || refreshTokenObj.expirationDate < moment().unix())
+  ) {
+    res.status(400).json({
+      error: {
+        code: 'INVALID_TOKEN',
+        message: 'Your refresh token is invalid or has expired.',
+      },
+      data: null,
+    });
+
+    return;
   }
 
   const user = await findOneBy<User>({ model: UserModel, condition: { _id: refreshTokenObj.userId } });
 
   if (!user) {
-    return res.status(404).json({
-      error: { code: 'USER_DOESNT_EXIST', message: '' },
+    res.status(404).json({
+      error: {
+        code: 'USER_NOT_FOUND',
+        message: 'We did not foud any user attach to this refresh token.',
+      },
       data: null,
     });
+
+    return;
   }
 
   const newToken = await createTokenForUser(user);
   const newRefreshToken = await createRefreshTokenForUser(user);
 
+  if (!newRefreshToken) {
+    res.status(500).json({
+      error: {
+        code: 'UNKNOWN_ERROR',
+        message: 'An error has occurred while saving the refresh token.',
+      },
+      data: null,
+    });
+
+    return;
+  }
+
   res.status(200).json({
     error: null,
-    data: { token: newToken, refreshToken: newRefreshToken!.token },
+    data: {
+      token: newToken,
+      refreshToken: newRefreshToken.token,
+    },
   });
 };
 
 export const logout = async (req: Request, res: Response) => {
   const { refreshToken } = req.body;
 
-  const deletionResult = await deleteOnyBy<RefreshToken>({ model: RefreshTokenModel, condition: { token: refreshToken } });
-
-  if (!deletionResult) {
-    res.status(200).json({
-      error: null,
-      data: {
-        message: 'You can close your browser',
-      },
-    });
-  } else {
-    console.log('[ERROR] impossible to delete refresh token');
-    console.log(deletionResult);
-    res.status(500).json({
+  if (!refreshToken) {
+    res.status(400).json({
       error: {
-        code: 'UNKNOWN_ERROR',
-        message: 'impossible to delete refresh token',
+        code: 'REFRESH_TOKEN_REQUIRED',
+        message: 'Please fill refreshToken field, this field is required.',
       },
       data: null,
     });
+    return;
   }
+
+  await deleteOnyBy<RefreshToken>({ model: RefreshTokenModel, condition: { token: refreshToken } });
+
+  res.status(204).json();
 };
