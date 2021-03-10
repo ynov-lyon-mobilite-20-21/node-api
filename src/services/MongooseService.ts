defe6cf5ca97cad9970c10bff02da67f290d8065
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2021-03-10 11:50:15 +0100

    refactor: MongooseService to return result

--- src/services/MongooseService.ts
+++ src/services/MongooseService.ts
@@ -21,12 +21,48 @@ type UpdateByParams<T extends Document> = ByParams<T> & {
   set: object;
 }
 
+// TODO: Surround try/catch all db request
+// TODO: change returned data
 export const findOneBy = async <T extends Document>({ model: ModelObject, condition, hiddenPropertiesToSelect }: FindByParams<T>): Promise<T | null> => {
   try {
     // @ts-ignore
-    const finedObject = await ModelObject.findOne(condition)
+    const objectFound = await ModelObject.findOne(condition)
       .select(hiddenPropertiesToSelect ? hiddenPropertiesToSelect.map((property) => `+${property}`) : '');
-    return finedObject || null;
+    return objectFound || null;
+  } catch (e) {
+    return null;
+  }
+};
+
+export const findManyBy = async <T extends Document>({ model: ModelObject, condition, hiddenPropertiesToSelect }: FindByParams<T>): Promise<T[] | null> => {
+  try {
+    // @ts-ignore
+    return ModelObject.find(condition)
+      .select(hiddenPropertiesToSelect ? hiddenPropertiesToSelect.map((property) => `+${property}`) : '');
+  } catch (e) {
+    return null;
+  }
+};
+
+export const updateOneBy = async <T extends Document>({ model: ModelObject, condition, set }: UpdateByParams<T>): Promise<T | null> => {
+  try {
+    // @ts-ignore
+    const updatedObject = await ModelObject.updateOne(condition, { $set: set, $inc: { __v: 1 } }); // TODO: implement incrementation
+
+    // @ts-ignore
+    return findOneBy<typeof ModelObject>({ model: ModelObject, condition: { _id: updatedObject._id } });
+  } catch (e) {
+    return null;
+  }
+};
+
+export const updateManyBy = async <T extends Document>({ model: ModelObject, condition, set }: UpdateByParams<T>): Promise<T | null> => {
+  try {
+    // @ts-ignore
+    const updatedObject = await ModelObject.updateMany(condition, { $set: set, $inc: { __v: 1 } }); // TODO: implement incrementation
+
+    // @ts-ignore
+    return findOneBy<typeof ModelObject>({ model: ModelObject, condition: { _id: updatedObject._id } });
   } catch (e) {
     return null;
   }
@@ -44,39 +80,9 @@ export const saveData = async <T extends Document>({ model: ModelObject, params 
 export const deleteOnyBy = async <T extends Document>({ model: ModelObject, condition }: ByParams<T>): Promise<boolean> => {
   try {
     // @ts-ignore
-    const deletion = await ModelObject.deleteOne(condition);
-    return deletion.deletedCount! > 0;
+    const deleteResult = await ModelObject.deleteOne(condition);
+    return deleteResult.deletedCount! > 0;
   } catch (e) {
     return false;
-  }
-};
-
-export const updateOneBy = async <T extends Document>({ model: ModelObject, condition, set }: UpdateByParams<T>): Promise<boolean> => {
-  try {
-    // @ts-ignore
-    const update = await ModelObject.updateOne(condition, { $set: set, $inc: { __v: 1 } });
-    return update.nModified > 0;
-  } catch (e) {
-    return false;
-  }
-};
-
-export const updateManyBy = async <T extends Document>({ model: ModelObject, condition, set }: UpdateByParams<T>): Promise<boolean> => {
-  try {
-    // @ts-ignore
-    const update = await ModelObject.updateMany(condition, { $set: set, $inc: { __v: 1 } });
-    return update.nModified > 0;
-  } catch (e) {
-    return false;
-  }
-};
-
-export const findManyBy = async <T extends Document>({ model: ModelObject, condition, hiddenPropertiesToSelect }: FindByParams<T>): Promise<T[]> => {
-  try {
-    // @ts-ignore
-    return ModelObject.find(condition)
-      .select(hiddenPropertiesToSelect ? hiddenPropertiesToSelect.map((property) => `+${property}`) : '');
-  } catch (e) {
-    return [];
   }
 };
