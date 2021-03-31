6e38af22f15f338e8ba0924cd59f1c069e69ae5e
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2021-03-31 15:30:11 +0200

    feat: trying to add stripe webhook for payment intent

--- src/controllers/StripeController.ts
+++ src/controllers/StripeController.ts
@@ -8,6 +8,28 @@ import {
 import { Card, CardModel } from '../models/CardModel';
 import { APIRequest } from '../Interfaces/APIRequest';
 
+export const webhookPaymentIntent = async (req: Request, res: Response): Promise<void> => {
+  const event = req.body;
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      console.log('PaymentIntent was successful!');
+      break;
+    case 'payment_method.attached':
+      console.log('PaymentMethod was attached to a Customer!');
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  console.log(event);
+
+  // Return a 200 response to acknowledge receipt of the event
+  res.status(200).json({ received: true });
+};
+
 export const linkCardToCurrentUser = async (req: Request, res: Response): Promise<void> => {
   const { stripeId } = req.body;
   const request = req as APIRequest;
